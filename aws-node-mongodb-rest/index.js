46e0ef2d51434384386883be558f03852811c6c3
TheCatalystAdmin <nehjoshi5@gmail.com>
2024-04-18 13:46:47 -0400

    Added helloWorldBackup

--- aws-node-mongodb-rest/index.js
+++ aws-node-mongodb-rest/index.js
@@ -1,75 +1,53 @@
-const mongoose = require('mongoose');
-const Promise = require('bluebird');
-const validator = require('validator');
-const UserModel = require('./model/User.js');
-const failureLambda = require('failure-lambda');
 const AWS = require('aws-sdk');
 
-mongoose.Promise = Promise;
-
-const mongoString = 'mongodb+srv://admin-jivsaf:admin-jivsaf@jivan-safalya.k7ghr.mongodb.net/The_Mirror_Test'; // MongoDB Url
-
-const ssm = new AWS.SSM();
-
-async function getParameterValue(parameterName) {
-    try {
-        const response = await ssm.getParameter({ Name: parameterName }).promise();
-        return response.Parameter.Value;
-    } catch (error) {
-        console.error('Error retrieving parameter:', error);
-        throw error;
-    }
-}
-
-const createErrorResponse = (statusCode, message) => ({
-    statusCode: statusCode || 501,
-    headers: { 'Content-Type': 'text/plain' },
-    body: message || 'Incorrect id',
-});
-
-// const dbExecute = (db, fn) => db.then(fn).finally(() => db.close());
-
-async function dbConnectAndExecute(dbUrl) {
-    console.log(dbUrl);
-    try {
-        await mongoose.connect(dbUrl);
-        console.log("Connected");
-        // return true;
-    }
-    catch (err) {
-        console.log(err);
-        return err;
-    }
-}
-
+const statusMessages = {
+    200: "OK",
+    201: "Created",
+    204: "No Content",
+    400: "Bad Request",
+    401: "Unauthorized",
+    403: "Forbidden",
+    404: "Not Found",
+    405: "Method Not Allowed",
+    408: "Request Timeout",
+    500: "Internal Server Error",
+    502: "Bad Gateway",
+    503: "Service Unavailable"
+  };
 
 exports.handler = async (event, context) => {
     const lambda = new AWS.Lambda();
     const params = {
         FunctionName: 'hello_world', // Replace with the name of the target Lambda function
         InvocationType: 'RequestResponse', // Synchronous invocation
-        Payload: JSON.stringify({id: event.id}) // Pass any required input payload
+        Payload: JSON.stringify({ id: event.id }) // Pass any required input payload
     };
     try {
-        console.log("Entered try block");
+        console.log("Invoking hello_world...");
         const res = await lambda.invoke(params).promise(); //Invoke first function to get the user
-        console.log("Line 2");
-        const response = JSON.parse(res.Payload);
-        console.log("First res: ",  response);
+        const response = await JSON.parse(res.Payload);
+        console.log("Response from hello_world: ", response);
+
+        if (response.statusCode && Number(response.statusCode) !== 200) return {
+            statusCode: response.statusCode,
+            message: statusMessages[Number(response.statusCode)]
+        }
+
+        if (!response.user) return {
+            statusCode: 500,
+            message: "Internal Server Error, please try again"
+        };
 
         const updateParams = {
             FunctionName: 'db_updates', // Replace with the name of the target Lambda function
             InvocationType: 'RequestResponse', // Synchronous invocation
-            Payload: JSON.stringify({id: response.user[0]._id, newEmail: event.newEmail}) // Pass any required input payload
+            Payload: JSON.stringify({ id: response.user[0]._id, newEmail: event.newEmail }) // Pass any required input payload
         };
-        console.log("Line 4: Update Params: ", updateParams);
+        console.log("Invoking db_updates... ", updateParams);
 
-        const resUpdate = await lambda.invoke(updateParams).promise();
-        console.log("Line 5");
+        const resUpdate = await lambda.invoke(updateParams).promise(); //Invoke second function to update user's email
         const responseUpdate = JSON.parse(resUpdate.Payload);
-        console.log("Line 6, responseUpdate: ", responseUpdate);
-
-        console.log("Updated User: ", responseUpdate);
+        console.log("Response from db_updates: ", responseUpdate);
         return responseUpdate;
     }
     catch (e) {
