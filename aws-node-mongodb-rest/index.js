74191627ac1d9bf72ff6cd0f666efb2a9c927155
TheCatalystAdmin <nehjoshi5@gmail.com>
2024-04-18 16:07:20 -0400

    Added retry mechanism + backup connections

--- aws-node-mongodb-rest/index.js
+++ aws-node-mongodb-rest/index.js
@@ -13,20 +13,59 @@ const statusMessages = {
     500: "Internal Server Error",
     502: "Bad Gateway",
     503: "Service Unavailable"
-  };
+};
+
+const getParams = (functionName, payload) => {
+    return {
+        FunctionName: functionName,
+        InvocationType: 'RequestResponse',
+        Payload: JSON.stringify(payload)
+    }
+}
 
 exports.handler = async (event, context) => {
     const lambda = new AWS.Lambda();
-    const params = {
-        FunctionName: 'hello_world', // Replace with the name of the target Lambda function
-        InvocationType: 'RequestResponse', // Synchronous invocation
-        Payload: JSON.stringify({ id: event.id }) // Pass any required input payload
-    };
+    const helloWorldParams = getParams("hello_world", { id: event.id });
+    const backupDBParams = getParams("backupDB", { id: event.id });
+    console.log(helloWorldParams);
+
+    let res = await lambda.invoke(helloWorldParams).promise();
+    let response = JSON.parse(res.Payload);
+    let n = 1;
+    while (!response.user && n <= 3) { //Mechanism to retry function call 3 times
+        res = await lambda.invoke(helloWorldParams).promise();
+        response = JSON.parse(res.Payload);
+        console.log("Current iteration: ", n);
+        n += 1;
+    }
+
+    if (!response.user) return {status: 500, message: "Internal server error"}
+    console.log("Final response: ", response);
+    return response;
+
+
+
+    // const res = await lambda.invoke(helloWorldParams).promise(); //If A doesn't work, try B
+    // const response = JSON.parse(res.Payload);
+    // if (!response.user) {
+    //     const res2 = await lambda.invoke(backupDBParams).promise();
+    //     const response2 = JSON.parse(res2.Payload);
+    //     console.log("Backup function response: ", response2);
+    //     return response2;
+    // }
+    // else {
+    //     console.log("Original function response: ", response);
+    //     return response;
+    // }
+    
+
     try {
         console.log("Invoking hello_world...");
         const res = await lambda.invoke(params).promise(); //Invoke first function to get the user
         const response = await JSON.parse(res.Payload);
         console.log("Response from hello_world: ", response);
+
+
 
         if (response.statusCode && Number(response.statusCode) !== 200) return {
             statusCode: response.statusCode,
@@ -38,17 +77,21 @@ exports.handler = async (event, context) => {
             message: "Internal Server Error, please try again"
         };
 
-        const updateParams = {
-            FunctionName: 'db_updates', // Replace with the name of the target Lambda function
-            InvocationType: 'RequestResponse', // Synchronous invocation
-            Payload: JSON.stringify({ id: response.user[0]._id, newEmail: event.newEmail }) // Pass any required input payload
-        };
-        console.log("Invoking db_updates... ", updateParams);
 
-        const resUpdate = await lambda.invoke(updateParams).promise(); //Invoke second function to update user's email
-        const responseUpdate = JSON.parse(resUpdate.Payload);
-        console.log("Response from db_updates: ", responseUpdate);
-        return responseUpdate;
+
+
+
+        // const updateParams = {
+        //     FunctionName: 'db_updates', // Replace with the name of the target Lambda function
+        //     InvocationType: 'RequestResponse', // Synchronous invocation
+        //     Payload: JSON.stringify({ id: response.user[0]._id, newEmail: event.newEmail }) // Pass any required input payload
+        // };
+        // console.log("Invoking db_updates... ", updateParams);
+
+        // const resUpdate = await lambda.invoke(updateParams).promise(); //Invoke second function to update user's email
+        // const responseUpdate = JSON.parse(resUpdate.Payload);
+        // console.log("Response from db_updates: ", responseUpdate);
+        // return responseUpdate;
     }
     catch (e) {
         return {
---
+++ backupDB/index.js
@@ -0,0 +1,53 @@
+const failureLambda = require('failure-lambda');
+const AWS = require('aws-sdk');
+const mongoose = require('mongoose');
+const Promise = require('bluebird');
+const validator = require('validator');
+const UserModel = require('./model/User.js');
+
+const ssm = new AWS.SSM();
+
+async function dbConnectAndExecute(dbUrl) { //Establish connection with DB
+    console.log(dbUrl);
+    try {
+        await mongoose.connect(dbUrl);
+        console.log("Connected");
+        // return true;
+    }
+    catch (err) {
+        console.log(err);
+        return err;
+    }
+}
+
+
+async function getParameterValue(parameterName) { //Get SSM Parameter values
+    try {
+        const response = await ssm.getParameter({ Name: parameterName }).promise();
+        return response.Parameter.Value;
+    } catch (error) {
+        console.error('Error retrieving parameter:', error);
+        throw error;
+    }
+}
+
+exports.handler = async (event, context, callback) => {
+    // const failureLambdaParam = process.env.FAILURE_INJECTION_PARAM;
+    // const parameterValue = await getParameterValue(failureLambdaParam);
+    // Access the id parameter
+    console.log(event.id);
+    await dbConnectAndExecute(process.env.DB_URL);
+    try {
+        const user = await UserModel.find({ _id: event.id }); //Access the id param provided by the invoking function
+        return {
+            statusCode: 200,
+            user: user
+        }
+    }
+    catch (e) {
+        return {
+            statusCode: 500,
+            message: "Internal Server Error"
+        }
+    }
+}
